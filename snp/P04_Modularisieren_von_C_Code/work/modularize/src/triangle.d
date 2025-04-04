4eed918a7b91d4c9b356a0b48335aef1b2f96802
Shengli Yan <yanshe01@students.zhaw.ch>
2025-04-04 15:54:17 +0200

    m

--- snp/P04_Modularisieren_von_C_Code/work/modularize/src/triangle.d
+++ snp/P04_Modularisieren_von_C_Code/work/modularize/src/triangle.d
@@ -1,4 +1,4 @@
-tests/tests.o: tests/tests.c /usr/include/stdc-predef.h \
+src/triangle.o: src/triangle.c /usr/include/stdc-predef.h \
  /usr/include/stdio.h \
  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
  /usr/include/features.h /usr/include/features-time64.h \
@@ -22,22 +22,5 @@ tests/tests.o: tests/tests.c /usr/include/stdc-predef.h \
  /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
  /usr/include/x86_64-linux-gnu/bits/floatn.h \
  /usr/include/x86_64-linux-gnu/bits/floatn-common.h /usr/include/stdlib.h \
- /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
- /usr/include/CUnit/Basic.h /usr/include/CUnit/CUnit.h \
- /usr/include/string.h /usr/include/math.h \
- /usr/include/x86_64-linux-gnu/bits/math-vector.h \
- /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
- /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
- /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
- /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
- /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
- /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
- /usr/include/CUnit/CUError.h /usr/include/errno.h \
- /usr/include/x86_64-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
- /usr/include/x86_64-linux-gnu/asm/errno.h \
- /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
- /usr/include/CUnit/TestDB.h /usr/include/setjmp.h \
- /usr/include/x86_64-linux-gnu/bits/setjmp.h \
- /usr/include/x86_64-linux-gnu/bits/types/struct___jmp_buf_tag.h \
- /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
- /usr/include/CUnit/TestRun.h include/test_utils.h
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h src/read.h \
+ src/rectang.h
